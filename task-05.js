1309dc22e85ac7b5ef6305b68f2ac8a68ecaab53
Roman <Roman.Vengl1@gmail.com>
2019-10-25 15:58:59 +0300

    task-5

---
+++ task-05.js
@@ -0,0 +1,12 @@
+const link = {
+  input: document.querySelector('input'),
+  output: document.querySelector('#name-output'),
+};
+
+const handleInput = e => {
+  e.preventDefault();
+  const { target } = e;
+  link.output.textContent = target.value || 'незнакомец';
+};
+
+link.input.addEventListener('input', handleInput);
